bd7e58958b236d3ee90f58e684a0b121d956899f
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2025-01-26 21:12:52 +0900

    fix: step3 TODO 추가

--- dbms/src/main/kotlin/dbms/query/WhereClauseBuilder.kt
+++ dbms/src/main/kotlin/dbms/query/WhereClauseBuilder.kt
@@ -3,13 +3,9 @@ package dbms.query
 class WhereClauseBuilder {
     private val clauses = mutableMapOf<String, WhereClause>()
 
-    infix fun String.equalsTo(other: Any) {
-        clauses[this] = WhereClause(this, Operator.EQUALS, other)
-    }
+    // TODO 1. equalsTo 함수 정의 - 문자열 혹은 숫자 속성을 가진 특정 필드가 특정 문자열 혹은 숫자와 같은 값을 가지는지 판단
 
-    infix fun String.like(other: Any) {
-        clauses[this] = WhereClause(this, Operator.LIKE, other)
-    }
+    // TODO 2. like 함수 정의 - 문자열 속성을 가진 특정 필드가 특정 문자열을 포함하는지 판단
 
     internal fun build(): List<WhereClause> = clauses.map { it.value }
 }
