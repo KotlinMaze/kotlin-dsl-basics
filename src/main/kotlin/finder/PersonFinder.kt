3bdc4bbda9fea6b43ec171bc15bffcb8a1aec654
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2025-01-26 19:51:49 +0900

    feat: 테이블 생성 로직 DSL 구현

--- src/main/kotlin/finder/PersonFinder.kt
+++ src/main/kotlin/finder/PersonFinder.kt
@@ -1,13 +1,15 @@
 package finder
 
+import dbms.ui.create
+
 fun main() {
+    create {
+        table("Person")
+        attributes("name" to "string", "age" to "int")
+        values("Kame" x 28, "Km" x 17, "Kate" x 25)
+    }
+
 //    TODO : 아래 코드 주석을 해제했을 때, 올바르게 동작하도록 구현
-//    create {
-//        table("Person")
-//        attributes("name" to "string", "age" to "int")
-//        values("Kame" x 28, "Km" x 17, "Kate" x 25)
-//    }
-//
 //    select {
 //        columns("name", "age")
 //        from("Person")
